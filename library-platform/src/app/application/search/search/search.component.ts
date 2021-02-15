5f76cf4b95a29c7f2d7c10bef542b7019bf4b6a2
pranjkic <49720920+pranjkic@users.noreply.github.com>
2021-02-15 21:18:30 +0100

    adding first service

--- library-platform/src/app/application/search/search/search.component.ts
+++ library-platform/src/app/application/search/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import {LibraryService} from '../../../services/library.service'
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private libraryService: LibraryService) { }
 
   ngOnInit(): void {
+    console.log(this.libraryService.demoMethod())
   }
 
 }
---
+++ library-platform/src/app/services/library.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LibraryService {
+
+  constructor() { }
+
+  demoMethod()
+  {
+    return "Hello World!"
+  }
+
+}
